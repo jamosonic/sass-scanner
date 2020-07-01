93365f7d5bf1cc931a7865e03d0afbc06020b13c
joshamos <josh.amos@99designs.com>
2020-07-02 08:09:07 +1000

    Try change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 
 export async function run(): Promise<void> {
   try {
-    core.setOutput('sass-file-count', '253')
+    core.setOutput('sass-file-count', '254')
   } catch (error) {
     core.setFailed(error.message)
   }
