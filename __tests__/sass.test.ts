dc48f4b937956ddb3ba190e4202aabcabeaea95f
joshamos <josh.amos@99designs.com>
2020-07-08 09:02:32 +1000

    Upgrade things

--- __tests__/sass.test.ts
+++ __tests__/sass.test.ts
@@ -5,7 +5,6 @@ jest.mock('@actions/core')
 describe('getSassGraph', () => {
   test('returns a sass graph', async () => {
     const graph = await getSassGraph('__tests__/fixtures')
-
     expect(Object.keys(graph.index).length).toBe(6)
   })
 })
