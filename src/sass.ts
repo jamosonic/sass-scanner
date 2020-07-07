a954812d5cb24b01cda6b2e1361f04e2be11622e
joshamos <josh.amos@99designs.com>
2020-07-07 23:44:01 +1000

    Fix tsconfig and linting

--- src/sass.ts
+++ src/sass.ts
@@ -1,6 +1,5 @@
-import {Graph} from 'sass-graph'
+import {Graph, parseDir} from 'sass-graph'
 import * as core from '@actions/core'
-import {parseDir} from 'sass-graph'
 
 export async function getSassGraph(dirPath: string): Promise<Graph> {
   const sassGraph = parseDir(dirPath)
