e6698789ccc14e8f2bc185a20042e0231ca9282a
joshamos <josh.amos@99designs.com>
2020-07-08 21:42:52 +1000

    Remove temporary sass-file-count output

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import * as core from '@actions/core'
 import {getSassGraph} from './sass'
 import {createCheck} from './github'
+import {buildReport} from './report'
 
 export async function run(): Promise<void> {
   try {
     const graph = await getSassGraph('./')
-    const sassFileCount = Object.keys(graph.index).length.toString()
-
-    core.setOutput(
-      'sass-file-count',
-      Object.keys(graph.index).length.toString()
-    )
-
+    const report = buildReport(graph)
     const token = core.getInput('repo-token')
-    await createCheck({token, summary: `${sassFileCount} sass files left`})
+    await createCheck({token, report})
   } catch (error) {
     core.setFailed(error.message)
   }
